40d6d6c4530c6ee51a059c23d596422983ef3e4d
tanisha-7001 <tanisha7001@gmail.com>
2024-05-26 20:47:25 +0530

    9

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,11 @@
+import { Amplify } from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
+
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import HomePage from './HomePage';
 import React from 'react';
 import '@aws-amplify/ui-react/styles.css';
-import { Amplify } from 'aws-amplify';
-import config from './aws-exports.js';
-Amplify.configure(config);
 
 
 function Login({ signOut, user }) {
