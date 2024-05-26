802d1ccb83d4d10b587f4223484553e3d949ef20
tanisha-7001 <tanisha7001@gmail.com>
2024-05-26 20:44:32 +0530

    9

--- src/Login.js
+++ src/Login.js
@@ -3,7 +3,7 @@ import HomePage from './HomePage';
 import React from 'react';
 import '@aws-amplify/ui-react/styles.css';
 import { Amplify } from 'aws-amplify';
-import config from './aws-exports';
+import config from './aws-exports.js';
 Amplify.configure(config);
 
 
